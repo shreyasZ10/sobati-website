3e4948cefcbc1a993d5bc8f4f155614bf1a504eb
Shreyas Deepak Zadrao <49271106+shreyasZ10@users.noreply.github.com>
2024-03-25 00:40:37 +0530

    feat: added our team page (#7)

---
+++ src/components/teamMember.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const TeamMember = ({ name, position, contactNumber, photo }) => {
+  return (
+    <div className="card team-member">
+      <div className="row no-gutters align-items-center justify-content-center">
+        <div className="col-md-4 text-center">
+          <img
+            src={photo}
+            className="card-img"
+            alt={name}
+            style={{ maxWidth: '60%', height: 'auto' }}
+          />
+        </div>
+        <div className="col-md-8">
+          <div className="card-body">
+            <h5 className="card-title">नाव: {name}</h5> {/* Name */}
+            <p className="card-text">पद: {position}</p> {/* Position */}
+            <p className="card-text">संपर्क क्र.: {contactNumber}</p> {/* Contact Number */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TeamMember;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import GalleryPage from './pages/GalleryPage';
 import ArticlePage from './pages/ArticlesPage';
+import OurTeamPage from './pages/OurTeamPage';
 
 ReactDOM.render(
   <Router>
@@ -11,6 +12,7 @@ ReactDOM.render(
       <Route exact path="/" element={<App />} />
       <Route path="/gallery" element={<GalleryPage />} /> {/* Define route for GalleryPage */}
       <Route path="/articles" element={<ArticlePage />} />
+      <Route path="/ourteam" element={<OurTeamPage />} />
     </Routes>
   </Router>,
   document.getElementById('root')
